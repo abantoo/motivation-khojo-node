862c63a169e7f7e9a4c97e8bd9393a0e5e8bd02c
abantoo <abantonaimur@gmail.com>
2023-06-29 04:52:52 +0600

    adds user realted routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import PostController from "../controller/postController.js";
+import UserController from "../controller/userController.js";
 
 const router = express.Router();
 
-router.get("/", PostController.getAllPosts);
 
-router.post("/", PostController.savePost);
+/**
+ * Routes related to post
+*/
 
-router.put('/', PostController.editPost);
+router.get("/post", PostController.getAllPosts);
+router.post("/post", PostController.savePost);
+router.put('/post', PostController.editPost);
+
+/**
+ * Routes related to user
+*/
+
+router.get("/users", UserController.getUsers);
+router.get("/user/:id", UserController.getUser);
+router.post("/user", UserController.createUser);
+router.put("/user", UserController.updateUser);
+router.delete("user/:id", UserController.deleteUser);
 
 export default router;
