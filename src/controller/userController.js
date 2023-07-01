c1b5ddea4bddf7f5b68fb382830f710e5536a03b
abantoo <abantonaimur@gmail.com>
2023-07-01 23:32:09 +0600

    updates user controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,28 +1,40 @@
 export default class UserController {
     static async createUser(req, res) {
-
         const email = req.body.email;
         const password = req.body.password;
         const username = req.body.username;
 
-        const user = await UserModel.create({ email, username, password });
+        const user = await UserModel.createUser({ email, username, password });
 
-        if (user) {
+        if (!user) {
             res.json({
-                message: `successfully created user ${user}`,
+                message: "error while creating user"
             });
+
+            return;
         }
+
+        res.json({
+            message: `successfully created user ${user}`,
+        });
     };
 
     static async updateUser(req, res) {
-        res.send("hello");
-        // try {
-        //     const result = FeedModel.getAllMotivations();
-        //     res.send(result);
-        //     console.log("Successfully fetched all motivations from database");
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        const payload = req.body;
+
+        const updatedUser = await UserModel.updateUser(payload);
+
+        if (!updatedUser) {
+            res.json({
+                message: "error while updating user"
+            });
+            return;
+        }
+
+        res.json({
+            message: `successfully updated user ${updatedUser}`,
+        });
+
     };
 
     static async getUsers(req, res) {
